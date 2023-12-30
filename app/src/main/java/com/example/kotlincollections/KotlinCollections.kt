e31633cda58f17f4b376060167995f8133e0d453
Swapnil Nangare <swapnilnangare143@gmail.com>
2023-12-30 07:13:48 +0530

    Kotlin Collections

---
+++ app/src/main/java/com/example/kotlincollections/KotlinCollections.kt
@@ -0,0 +1,15 @@
+package com.example.kotlincollections
+
+fun main() {
+    println("Kotlin Collections")
+
+    /*
+    * Types of Kotlin Collections 1-> Immutable Collection (or Collection) 2-> Mutable Collection
+    * Immutable collection also called Collection supports read only functionalities
+    * 1.Immutable Collection:-> List,Map,Set
+    * 2.Mutable Collection:-> Mutable collections supports both read and write functionalities. -> List,Map,Set
+    *
+    * */
+
+
+}
