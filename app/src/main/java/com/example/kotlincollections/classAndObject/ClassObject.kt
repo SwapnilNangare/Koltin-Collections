a6317dd747c53d2e8c6af0873589d36505cbb734
Swapnil Nangare <swapnilnangare143@gmail.com>
2024-01-05 15:45:37 +0530

    Kotlin Nested class and Inner class

--- app/src/main/java/com/example/kotlincollections/classAndObject/ClassObject.kt
+++ app/src/main/java/com/example/kotlincollections/classAndObject/ClassObject.kt
@@ -1,6 +1,65 @@
 package com.example.kotlincollections.classAndObject
 
 fun main() {
+    var obj1 = Account()
+//    obj1.acc_no = 123
+//    obj1.name = "Swapnil"
+//    obj1.amount = 1000000000.233F
+
+    obj1.insert(832345,"Ankit",1000000000.233f) //accessing member function
+    println("${obj1.name}") //accessing class property
+
 
 
 }
+
+class Animal {
+    // class header
+    // property
+    // member function
+}
+
+// class with constructor
+
+class className constructor() {  // class header
+    // property
+    // member function
+
+}
+
+class Account {
+    var acc_no: Int = 0
+    var name: String? = null
+    var amount: Float = 0f
+
+
+    fun insert(ac: Int, n: String, am: Float) {
+        acc_no = ac
+        name = n
+        amount = am
+        println("Account no: ${acc_no} holder :${name} amount :${amount}")
+    }
+
+    fun deposit() {
+        // deposit
+    }
+
+    fun withdraw() {
+        // withdraw code
+    }
+
+    fun checkBalance() {
+        // balance code
+    }
+
+    /* Kotlin Object
+    Object is real time entity or may be a logical entity which has state and behavior. It has the characteristics:
+
+    state: it represents value of an object.
+    behavior: it represent the functionality of an object.
+
+  */
+
+
+}
+
---
+++ app/src/main/java/com/example/kotlincollections/classAndObject/NestedClassAndInnerClass.kt
@@ -0,0 +1,31 @@
+package com.example.kotlincollections.classAndObject
+
+fun main() {
+
+    println("Nested class and Inner class")
+// nested class must be initialize
+    println(OuterClass.NestedClass()) // accessing property
+
+    var obj = OuterClass.NestedClass() // object creation
+    obj.foo() // access member function
+
+}
+
+class OuterClass {
+    //outer class code
+
+    private var name: String = "Ashu"
+
+
+    class NestedClass {
+        //nested class code
+        private var id: Int = 101
+
+        fun foo() {
+            //  print("name is ${name}") // cannot access the outer class member
+            println("Id is ${id}")
+        }
+
+    }
+
+}
