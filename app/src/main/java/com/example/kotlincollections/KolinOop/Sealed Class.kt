d9fdb3120c7d7496a3c7b481b7dcd503315f8cb5
Swapnil Nangare <swapnilnangare143@gmail.com>
2024-01-08 09:16:23 +0530

    Sealed Class

--- app/src/main/java/com/example/kotlincollections/KolinOop/Sealed Class.kt
+++ app/src/main/java/com/example/kotlincollections/KolinOop/Sealed Class.kt
@@ -2,4 +2,11 @@ package com.example.kotlincollections.KolinOop
 
 fun main() {
     println("Sealed Class")
+
+    /* Sealed class is a class which restricts the class hierarchy. A class can be declared as
+     sealed class using "sealed" keyword before the class name. It is used to represent restricted class hierarchy.
+     Sealed class is used when the object have one of the types from limited set, but cannot have any other type.
+     The constructors of sealed classes are private in default and cannot be allowed as non-private. */
+
+
 }
